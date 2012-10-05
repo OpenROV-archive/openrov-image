5d16838ca5e92e8b87f4c5c09c8ebdfafd6ffa48
Dominik Fretz <info@codewithpassion.com>
2012-10-05 16:56:22 +0000

    fixing

--- lib/install_openrov.sh
+++ lib/install_openrov.sh
@@ -1,15 +1,21 @@
 #/bin/sh
-
+echo currently in:
+echo `pwd`
 DIR="`dirname \"$0\"`"
 
 if [ ! -d /opt/openrov ]; then
 	mkdir -p /opt/openrov
 fi
-cp -r $DIR/openrov /opt/openrov
+cp -r /tmp/additions/openrov/openrov /opt/
+
+echo "ls /opt:"
+echo `ls /opt`
 
 echo "rov ALL=NOPASSWD: /opt/openrov/linux/*.sh" >> /etc/sudoers
 
 ln -s /opt/openrov/linux/openrov.service /etc/init.d/openrov
 
-update-rc.d openrov defaults
+if [ -f '/etc/init.d/openrov' ]; then
+	update-rc.d openrov defaults
+fi  
 
