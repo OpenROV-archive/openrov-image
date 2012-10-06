4b75f011bdbfbe5d8a4dc95fb24de4e611d63f56
Dominik Fretz <info@codewithpassion.com>
2012-10-06 17:36:01 +0000

    change to hf versions of the compilers

--- setup-system.sh
+++ setup-system.sh
@@ -4,6 +4,6 @@ sudo add-apt-repository ppa:richarvey/nodejs
 sudo add-apt-repository ppa:linaro-maintainers/toolchain
 sudo apt-get update
 
-sudo apt-get install gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm gcc-arm-linux-gnueabi g++-arm-linux-gnueabi fakeroot
+sudo apt-get install gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm gcc-4.6-arm-linux-gnueabihf g++-4.6-arm-linux-gnueabihf fakeroot
 
 
