d14b35f51ac23e456ae653021c2617ed384d1643
Dominik Fretz <info@codewithpassion.com>
2012-10-25 19:06:38 +0200

    improve openrov installer

--- lib/install_openrov.sh
+++ lib/install_openrov.sh
@@ -18,6 +18,30 @@ rm -rf package
 
 echo "rov ALL=NOPASSWD: /opt/openrov/linux/" >> /etc/sudoers
 
+echo > /etc/rc.local << __EOF__
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+sh /opt/openrov/linux/seturat.sh
+
+exit 0
+
+__EOF__
+
+
+
+
+
 ln -s /opt/openrov/linux/openrov.service /etc/init.d/openrov
 chmod +x /opt/openrov/linux/openrov.service
 
