6c05e78338779c66a7fa73ed46103041bb0b60c0
codewithpassion <dominik.fretz@gmail.com>
2013-12-26 00:19:35 +1100

    Fixing emmc install scripts

--- steps/step_03/emmc-beforeremove.sh
+++ steps/step_03/emmc-beforeremove.sh
@@ -1,5 +1,5 @@
-#!/usr/bin
+#!/bin/sh
 
-
+sed -i 's/^#\/opt\/openrov\/linux\/rc.local/\/opt\/openrov\/linux\/rc.local/g' /etc/rc.local
 sed -i '/\/opt\/openrov\/linux\/copy-to-emmc.sh/d ' /etc/rc.local
 sed -i '/dpkg -r openrov-emmc-copy/d ' /etc/rc.local
