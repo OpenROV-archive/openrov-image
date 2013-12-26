fae8b4a805dae657440db8ea60ce3d9a7f0bbb10
codewithpassion <dominik.fretz@gmail.com>
2013-12-26 19:16:18 +1100

    dpkg -r openrov-emmc-copy is now in the copy script itself to make it execute in a chroot environment on the new root, otherwise the flasher sd card can only be used once

--- steps/step_03/emmc-beforeremove.sh
+++ steps/step_03/emmc-beforeremove.sh
@@ -2,5 +2,4 @@
 
 sed -i 's/^#\/opt\/openrov\/linux\/rc.local/\/opt\/openrov\/linux\/rc.local/g' /etc/rc.local
 sed -i '/\/opt\/openrov\/linux\/copy-to-emmc.sh || true/d ' /etc/rc.local
-sed -i '/dpkg -r openrov-emmc-copy/d ' /etc/rc.local
 sed -i '/^sync/d ' /etc/rc.local
