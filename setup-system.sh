7f241b088aba8cb21a8ee0d75f61a3fcb2277700
codewithpassion <dominik.fretz@gmail.com>
2014-02-21 21:32:17 +1100

    updating setup-system.sh to fix node and sh commands to the right things

--- setup-system.sh
+++ setup-system.sh
@@ -2,8 +2,9 @@
 
 sudo apt-get install dosfstools git-core kpartx u-boot-tools wget parted gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm fakeroot libjpeg-dev cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 
-sudo rm /bin/sh
-sudo ln -s /bin/bash /bin/sh
+sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+sudo update-alternatives --install /bin/sh sh /bin/bash 10
+
 sudo apt-get install rubygems
 sudo gem install fpm
 
