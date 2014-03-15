7050c527798b3add382dfeba1fb2ff961523ef3b
codewithpassion <dominik.fretz@gmail.com>
2014-03-15 22:20:53 +1100

    Adding p7zip to packages to be installed

--- setup-system.sh
+++ setup-system.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install dosfstools git-core kpartx u-boot-tools wget parted gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm fakeroot libjpeg-dev cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt-get install dosfstools git-core kpartx u-boot-tools wget parted gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm fakeroot libjpeg-dev cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf p7zip p7zip-full
 
 sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 sudo update-alternatives --install /bin/sh sh /bin/bash 10
