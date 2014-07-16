5c2284e48197e61f2dfb3b8b63651c6cf0777801
codewithpassion <dominik.fretz@gmail.com>
2014-07-16 07:28:12 +0200

    Try to fix the samba configurationissue take 2.1

--- steps/02_update_image.sh
+++ steps/02_update_image.sh
@@ -88,7 +88,7 @@ sudo apt-get -y install \
 apt-get -y remove apache2
 
 # Samba doesn't start on the chroot, we need to add an config section and reconfigure it, otherwise the openrov-samba-config packages failes to install
-
+echo Fixing samba 
 sed -i '/interfaces =/a interfaces = lo' /etc/samba/smb.conf
 sed -i '0,/interfaces =/{//d;}' /etc/samba/smb.conf
 dpkg --configure samba
