f663ccb527824f1295f4fab050efb3a3e7ccc943
codewithpassion <dominik.fretz@gmail.com>
2014-08-12 23:29:47 +1000

    Trying new fs options for more resillience

--- steps/02_update_image.sh
+++ steps/02_update_image.sh
@@ -96,6 +96,11 @@ dpkg --configure samba
 /etc/init.d/samba stop
 /etc/init.d/sshd stop
 
+echo Updating fstab for improved mount options
+sed -i 's/\/dev\/mmcblk0p2.*/\/dev\/mmcblk0p2  \/            ext4  data=writeback,nodiratime,noatime,norelatime  0  1/' /etc/fstav
+
+echo "FSCKFIX=no" >> /etc/default/rcS 
+
 __EOF__
 chmod +x $ROOT/tmp/update.sh
 
@@ -104,7 +109,9 @@ chroot $ROOT /tmp/update.sh
 rm $ROOT/tmp/update.sh
 
 echo Setting the root fs mode to data=writeback to minimise impact of suddenly loosing power
-tune2fs -o journal_data_writeback  $ROOT_media
+tune2fs -O ^has_journal  $ROOT_media
+#tune2fs -O ^has_journal -o journal_data_writeback  $ROOT_media
+#tune2fs -O ^has_journal -o ^journal_data_writeback /dev/mapper/loop0p2
 
 chroot_umount
 unmount_image
