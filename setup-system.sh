31d4a1a8655ce74461de4528aeb45740c8562322
codewithpassion <dominik.fretz@gmail.com>
2014-09-17 02:26:02 -0700

    Adding docker

--- setup-system.sh
+++ setup-system.sh
@@ -8,3 +8,7 @@ sudo update-alternatives --install "/bin/sh" "sh" "/bin/bash" 10
 npm install -g node-pre-gyp
 
 sudo gem install fpm
+
+sudo apt-get -y install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
