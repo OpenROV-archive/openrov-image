d33811acb03efaecfa83aa593142c7f38f712bc8
codewithpassion <dominik.fretz@gmail.com>
2014-09-17 00:22:56 -0700

    Fix setup-system.sh

--- setup-system.sh
+++ setup-system.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y dosfstools git-core kpartx u-boot-tools wget parted gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm fakeroot libjpeg-dev cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf p7zip p7zip-full
+sudo apt-get install -y dosfstools git-core kpartx u-boot-tools wget parted gcc g++ make qemu qemu-user-static libglib2.0-dev git nodejs npm fakeroot libjpeg-dev cpp-arm-linux-gnueabihf g++-arm-linux-gnueabihf p7zip p7zip-full nodejs-legacy ruby1.9.1-dev
 
-sudo update-alternatives --install -y "/usr/bin/node" "node" "/usr/bin/nodejs" 10
-sudo update-alternatives --install -y "/bin/sh" "sh" "/bin/bash" 10
+sudo update-alternatives --install "/usr/bin/node" "node" "/usr/bin/nodejs" 10
+sudo update-alternatives --install "/bin/sh" "sh" "/bin/bash" 10
 
-sudo apt-get install -y rubygems
+npm install -g node-pre-gyp
+
 sudo gem install fpm
