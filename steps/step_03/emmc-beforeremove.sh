0e196e7cd247c69d8152169b47db9bec66c22ab3
codewithpassion <dominik.fretz@gmail.com>
2014-09-17 01:45:24 -0700

    fixing emmc scripts

--- steps/step_03/emmc-beforeremove.sh
+++ steps/step_03/emmc-beforeremove.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i 's/^#\/opt\/openrov\/cockpit\/linux\/rc.local/\/opt\/openrov\/linux\/rc.local/g' /etc/rc.local
+sed -i 's/^#\/opt\/openrov\/cockpit\/linux\/rc.local/\/opt\/openrov\/cockpit\/linux\/rc.local/g' /etc/rc.local
 sed -i '/\/opt\/openrov\/cockpit\/linux\/copy-to-emmc.sh || true/d ' /etc/rc.local
 sed -i '/^sync/d ' /etc/rc.local
