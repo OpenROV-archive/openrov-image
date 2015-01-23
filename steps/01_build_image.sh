949b468e4cc1daaf3c781a8fa570b88c2d294cf4
BrianAdams <brian@babastik.com>
2015-01-22 16:51:17 -0800

    fix patch line endings

--- steps/01_build_image.sh
+++ steps/01_build_image.sh
@@ -34,10 +34,10 @@ sed -i 's/\[1024\*1700\]/\[1024*1900]/' setup_sdcard.sh
 
 # Add docker aware
 sed -i '1i\
-setup_sdcard.sh
-# If running inside Docker, make our nodes manually, because udev will not be working.
-if [[ -f /.dockerenv ]]; then
-	dmsetup --noudevsync mknodes
+setup_sdcard.sh\
+# If running inside Docker, make our nodes manually, because udev will not be working.\
+if [[ -f /.dockerenv ]]; then\
+	dmsetup --noudevsync mknodes\
 fi
 1,/kpartx -av ${media_loop}/d' setup_sdcard.sh
 
