952fab0c9a78fdd6101035ea07774c878c90985a
BrianAdams <brian@babastik.com>
2015-03-10 22:16:23 -0700

    bypass bower install

--- steps/04_customize_image.sh
+++ steps/04_customize_image.sh
@@ -82,7 +82,7 @@ set -e
 
 echo ------------------------------
 echo installing bower
-npm install -ddd -g bower
+#npm install -ddd -g bower
 
 echo Setting up users
 echo -----------------------------
