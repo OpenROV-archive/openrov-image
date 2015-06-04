d8d2bc27dc0575effdc692437d7434d30f994be2
BrianAdams <brian@babastik.com>
2015-06-04 14:01:24 -0700

    Removed additional boot customization

--- steps/04_customize_image.sh
+++ steps/04_customize_image.sh
@@ -190,9 +190,9 @@ echo Customizing boot partition
 # change boot script for uart
 #sed -i '3ioptargs=capemgr.enable_partno=BB-UART1' $DIR/boot/uEnv.txt
 
-mkdir $DIR/boot/Docs
-cp $DIR/contrib/openrov.ico $DIR/boot/Docs/
-cp $DIR/contrib/boot/* $DIR/boot/
+#mkdir $DIR/boot/Docs
+#cp $DIR/contrib/openrov.ico $DIR/boot/Docs/
+#cp $DIR/contrib/boot/* $DIR/boot/
 
 
 echo ------------------------------
